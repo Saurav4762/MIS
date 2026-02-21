808447641d2dc7213a7c7c5d1a44e7e21d04fc0e
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-02-21 12:59:30 +0545

    Add Model AppUser

--- MIS.API/Models/AppUser.cs
+++ MIS.API/Models/AppUser.cs
@@ -1,8 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MIS.API.Models
 {
     public class AppUser
     {
-        public Guid UserId { get; set; }
+        [Key]
+        public Guid Id { get; set; }
         public string? Username { get; set; }
         public string? FullName { get; set; }
         public string? Phone { get; set; }
@@ -11,8 +14,5 @@ namespace MIS.API.Models
         public bool IsActive { get; set; } = true;
         public DateTime CreatedAt { get; set; }
 
-        // Navigation properties
-        public ICollection<AppUserRole> UserRoles { get; set; } = new List<AppUserRole>();
-        public ICollection<Submission> Submissions { get; set; } = new List<Submission>();
     }
 }
