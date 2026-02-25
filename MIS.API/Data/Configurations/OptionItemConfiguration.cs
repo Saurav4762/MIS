b5c207d3576470a0469a6fa7611fa022822b44e3
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-02-25 16:44:28 +0545

    Add OptionItem Configuration

---
+++ MIS.API/Data/Configurations/OptionItemConfiguration.cs
@@ -0,0 +1,24 @@
+namespace MIS.API.Data.Configurations;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MIS.API.Models;
+
+public class OptionItemConfiguration : IEntityTypeConfiguration<OptionItem>
+{
+  public void Configure(EntityTypeBuilder<OptionItem> entity)
+  {
+    entity.HasKey(e => e.OptionList);
+
+    entity.Property(e => e.LabelEn).IsRequired().HasMaxLength(200);
+    entity.Property(e => e.LabelNe).IsRequired().HasMaxLength(200);
+
+
+    entity.HasOne(e => e.OptionList)
+          .WithMany(e => e.OptionItems)
+          .HasForeignKey(e => e.OptionListId)
+          .OnDelete(DeleteBehavior.Cascade);
+
+
+  }
+}
