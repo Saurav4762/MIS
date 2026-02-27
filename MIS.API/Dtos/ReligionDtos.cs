163a3e3216835ee1dc22bfb89e86375ff727e7d5
Saurav4762 <upretysaurav4@gmail.com>
2026-02-27 16:52:44 +0545

    Dtos: Religion Dtos Created

--- MIS.API/Dtos/ReligionDtos.cs
+++ MIS.API/Dtos/ReligionDtos.cs
@@ -1,6 +1,22 @@
+using Mis.API.Models;
+
 namespace MIS.API.Dtos;
 
-public class ReligionDtos
+
+public class ReligionRequestDto
 {
+    public string NameEn { get; set; }
+    
+    public string NameNe { get; set; }
+    
+}
+
+public class ReligionResponseDto
+{
+    public Guid Id { get; set; }
+    
+    public string NameEn { get; set; }
+    
+    public string NameNe { get; set; }
     
 }
