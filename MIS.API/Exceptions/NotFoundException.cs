ae8a9a4cd4c3553371b9c434a1add19895157016
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-03-01 15:13:08 +0545

    Update NotFoundExceptions

--- MIS.API/Exceptions/NotFoundException.cs
+++ MIS.API/Exceptions/NotFoundException.cs
@@ -6,10 +6,15 @@ namespace MIS.API.Exceptions;
 
 public class NotFoundException : BaseException
 {
-  public NotFoundException(string entity, object key)
+  public NotFoundException(string entity, object key, object value)
   : base(
-      message: $"{entity} with identifier '{key}' was not found",
+      message: $"{entity} not found",
       statusCode: 404,
-      errorCode: "NOT_FOUND"
-  ) { }
+      errorCode: "NOT_FOUND",
+      new Dictionary<string, string[]>
+      {
+        {entity, [$"{key} with '${value}' was not found."]}
+      }
+  )
+  {}
 }
