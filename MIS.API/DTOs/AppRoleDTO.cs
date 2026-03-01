14702076b2376496f9032b0e98d9cb1419d6d743
Saurav4762 <upretysaurav4@gmail.com>
2026-03-01 12:20:06 +0545

    DTos: App role dto created

--- MIS.API/DTOs/AppRoleDTO.cs
+++ MIS.API/DTOs/AppRoleDTO.cs
@@ -2,5 +2,16 @@ namespace MIS.API.DTOs;
 
 public class AppRoleDTO
 {
-    
+     public class CreateRoleDto
+     {
+          public string RoleCode { get; set; } = string.Empty;
+          public string RoleName { get; set; } = string.Empty;
+     }
+
+     public class RoleResponseDto
+     {
+          public Guid Id { get; set; }
+          public string RoleCode { get; set; } = string.Empty;
+          public string RoleName { get; set; } = string.Empty;
+     }
 }
