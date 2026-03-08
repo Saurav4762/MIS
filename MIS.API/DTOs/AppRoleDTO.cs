ab2f048282910eeea8e3b5b218b79dc14bb01088
Saurav4762 <upretysaurav4@gmail.com>
2026-03-08 18:04:18 +0545

    DTO : AppRole Dto modified for user

--- MIS.API/DTOs/AppRoleDTO.cs
+++ MIS.API/DTOs/AppRoleDTO.cs
@@ -8,6 +8,13 @@ public class AppRoleDTO
           public string RoleName { get; set; } = string.Empty;
      }
 
+     public class AssignRoleDto
+     {
+          public Guid UserId {get;set;}
+          
+          public Guid RoleId {get;set;}
+     }
+
      public class RoleResponseDto
      {
           public Guid Id { get; set; }
