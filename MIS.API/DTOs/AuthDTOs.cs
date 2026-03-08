200d90fedb381c8f17bf2634ae9c119d677248c4
Saurav4762 <upretysaurav4@gmail.com>
2026-03-08 18:04:53 +0545

    DTO : Auth Dto modified for user

--- MIS.API/DTOs/AuthDTOs.cs
+++ MIS.API/DTOs/AuthDTOs.cs
@@ -3,28 +3,25 @@ using Mis.API.Models;
 
 namespace MIS.API.DTOs;
 
-public class AppUserDTO
+public class AuthDTOs
 {
-
-    public class RegisterDto
-    {
-        public string userName { get; set; } = null!;
-        public string FullName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string Phone {get; set;} = null!;
-        public string Password { get; set; } = null!;
-
-    }
-
-    public class LoginDto
+    public class LoginRequestDto
     {
         public string UserName {get; set;} = null!;
+        
         public string Password {get; set;} = null!;
     }
 
-    public class AuthResponseDto
+    public class LoginResponseDto
     {
         public string Token {get; set;} = null!;
+        
+        public string Username {get; set;} = null!;
+        
+        public string FullName {get; set;} = null!;
+        
         public DateTime ExpiresAt {get; set;}
+        
+        public List<string> Roles {get; set;} = null!;
     }
 }
