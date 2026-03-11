bfb3b3755485cb9aa69ea6a4ee51113065011b38
Saurav4762 <upretysaurav4@gmail.com>
2026-03-11 16:59:35 +0545

    Service:Added Validation in person service

--- MIS.API/Services/PeronService.cs
+++ MIS.API/Services/PeronService.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using MIS.API.DTOs;
+using MIS.API.Enum;
 using MIS.API.Exceptions;
 using MIS.API.Interfaces.IRepositories;
 using MIS.API.Interfaces.IServices;
 using MIS.API.Models;
+using static MIS.API.DTOs.PersonDTO;
 
 namespace MIS.API.Services;
 
@@ -15,8 +18,66 @@ public class PeronService : IPersonService
         _personRepo = personRepo;
     }
 
-    public async Task<PersonDTO.PersonResponseDto> CreateAsync(PersonDTO.CreatePersonDto dto)
+    private void ValidateCreatePerson(CreatePersonDto dto)
     {
+        var errors = new Dictionary<string, string[]>();
+        
+        if (string.IsNullOrWhiteSpace(dto.FirstName))
+            errors["FirstName"] = ["First name is required"];
+        
+        if(dto.FirstName.Any(char.IsDigit))
+            errors["FirstName"] = ["First name contains digits"];
+        
+        if(string.IsNullOrWhiteSpace(dto.MiddleName))
+            errors["MiddleName"] = ["Middle name is required"];
+        
+        if(dto.MiddleName.Any(char.IsDigit))
+            errors["MiddleName"] = ["Middle name contains digits"];
+        
+        if(string.IsNullOrWhiteSpace(dto.LastName))
+            errors["LastName"] = ["Last name is required"];
+        
+        if(dto.LastName.Any(char.IsDigit))
+            errors["LastName"] = ["Last name contains digits"];
+        
+        if (string.IsNullOrWhiteSpace(dto.Email))
+            errors["Email"] = ["Email is required"];
+        else
+        {
+            try
+            {
+                var emailCheck = new System.Net.Mail.MailAddress(dto.Email);
+
+            }
+            catch 
+            {
+               errors["Email"] = ["Email is required"];
+            }
+        }
+        
+        if(string.IsNullOrWhiteSpace(dto.BirthPlace))
+            errors["BirthPlace"] = ["Birth place is required"];
+        
+        if(string.IsNullOrWhiteSpace(dto.BloodGroup))
+            errors["BloodGroup"] = ["Blood group is required"];
+        
+        if(string.IsNullOrWhiteSpace(dto.FatherName))
+            errors["FatherName"] = ["Father name is required"];
+        
+        if(dto.DateOfBirth> DateTime.UtcNow)
+            errors["DateOfBirth"] = ["Date of birth is required"];
+        
+        if(!System.Enum.IsDefined(typeof(Gender), dto.Gender))
+        errors.Add("Gender", new[] {"Gender must be Male. Female or Others" });
+
+        if (errors.Any())
+            throw new ValidationException(errors);
+
+    }
+
+    public async Task<PersonResponseDto> CreateAsync(CreatePersonDto dto)
+    {
+        ValidateCreatePerson(dto);
 
         var person = new Person
         {
@@ -25,13 +86,13 @@ public class PeronService : IPersonService
             MiddleName = dto.MiddleName,
             LastName = dto.LastName,
             FatherName = dto.FatherName,
-            Gender = dto.Gender,
+            Gender =  dto.Gender,
             BloodGroup = dto.BloodGroup,
             Email = dto.Email,
             BirthPlace = dto.BirthPlace,
             DateOfBirth = dto.DateOfBirth
         };
-
+        
         await _personRepo.CreatePersonAsync(person);
 
         return new PersonDTO.PersonResponseDto
@@ -78,6 +139,7 @@ public class PeronService : IPersonService
 
     public async Task UpdateAsync(Guid id, PersonDTO.CreatePersonDto dto)
     {
+           var errors = new Dictionary<string, string[]>();
         var person = await _personRepo.GetPersonByIdAsync(id);
         
         if(person ==null )
@@ -98,11 +160,26 @@ public class PeronService : IPersonService
 
     public async Task DeleteAsync(Guid id)
     {
+        var errors = new Dictionary<string, string[]>();
         var person = await _personRepo.GetPersonByIdAsync(id); 
         
         if (person == null)
             throw new NotFoundException(entity: nameof(person), key: nameof(person.Id), value: id);
-        
-        await _personRepo.DeleteAsync(person);
+
+        try
+        {
+            await _personRepo.DeleteAsync(person);
+
+        }
+        catch (DbUpdateException dbEx)
+        {
+            errors["Deleted"] = ["Person cannot be deleted it is reference in another record"];
+
+        }
+        catch (Exception ex)
+        {
+            //Re-throw unexpected errors to global exception handler
+            throw;
+        }
     }
 }
